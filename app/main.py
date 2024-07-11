03e3f9d38c81e9fee9cccb11008f7bd5c121609b
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-11 12:40:00 +0300

    improve implementation of reports line formatting

--- app/main.py
+++ app/main.py
@@ -1,41 +1,23 @@
 def format_linter_error(error: dict) -> dict:
     return {
-        "column": error["column_number"], "line": error["line_number"],
-        "message": error["text"], "name": error["code"], "source": "flake8"
+        "column": error["column_number"],
+        "line": error["line_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            } for error in errors
-        ],
-        "path": file_path, "status": "failed"
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "passed" if len(errors) == 0 else "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [] if len(linter_report[reports]) == 0 else [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in linter_report[reports]
-            ],
-            "path": reports,
-            "status": "passed" if len(linter_report[reports]) == 0
-            else "failed"
-        }
-        for reports in linter_report
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
     ]
